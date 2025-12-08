c22b287f1f26e104926a80669b2956511f64a575
naylaapriliandita <zditay54c@gmail.com>
2025-12-08 20:15:28 +0800

    feat(books): Implementasi getAllBooks dan createBook

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -4,12 +4,55 @@ import { bookSchema } from '../validators/book.validator.js';
 
 // Fungsi untuk mendapatkan semua buku 
 export const getAllBooks = async (req, res) => {
-    return res.status(501).json({ message: 'GET ALL BOOKS: Not Implemented Yet' });
+    try {
+        const books = await prisma.book.findMany({
+            // Mengambil nama penulis dalam relasi
+            include: { author: { select: { name: true } } } 
+        });
+        res.status(200).json(books);
+    } catch (error) {
+        // Log error untuk debugging internal
+        console.error(error); 
+        res.status(500).json({ message: 'Gagal mengambil data buku.' });
+    }
 };
 
 // Fungsi untuk membuat buku baru 
 export const createBook = async (req, res) => {
-    return res.status(501).json({ message: 'CREATE BOOK: Not Implemented Yet' });
+    try {
+        const { error, value } = bookSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        const { title, isbn, stock, authorName } = value;
+
+        // 1. Cek atau Buat Penulis (Author) menggunakan upsert
+        const author = await prisma.author.upsert({
+            where: { name: authorName },
+            update: {},
+            create: { name: authorName },
+        });
+
+        // 2. Buat Buku
+        const newBook = await prisma.book.create({
+            data: {
+                title,
+                isbn,
+                stock,
+                authorId: author.id,
+                isAvailable: stock > 0
+            },
+            include: { author: { select: { name: true } } }
+        });
+
+        res.status(201).json({ message: 'Buku berhasil ditambahkan.', book: newBook });
+
+    } catch (error) {
+        if (error.code === 'P2002') { // Error jika ISBN duplikat
+            return res.status(409).json({ message: 'ISBN sudah terdaftar.' });
+        }
+        res.status(500).json({ message: 'Gagal membuat buku baru.' });
+    }
 };
 
 // Fungsi untuk mengupdate buku 
