7138b8b1ac4e4188b4f7294788805daafa880c3c
naylaapriliandita <zditay54c@gmail.com>
2025-12-14 00:28:42 +0800

    chore: register transaction routes di server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import bookRoutes from "./routes/book.routes.js";
+import authorRoutes from "./routes/author.routes.js";
+import transactionRoutes from "./routes/transaction.routes.js";
 
 dotenv.config();
 
@@ -12,6 +15,9 @@ app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/books", bookRoutes);
+app.use("/authors", authorRoutes);
+app.use("/transactions", transactionRoutes);
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
